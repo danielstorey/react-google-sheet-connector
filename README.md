71276388a0a07322a2a32542b14dc28719464fc1
Dan Storey <danielstorey@gmail.com>
2017-04-19 00:21:44 +0100

    Update files and documentation

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
 # react-google-sheet-connector
 Load data from a Google spreadsheet into your react components
+
+##Installation
+`npm install --save react-google-sheet-connector`
+
+##Overview
+Firstly the data must be initialized by using the `<ReactGoogleSheetConnector>` component. You then have the `<GoogleSheet>`, `<GoogleRoute>` and `<GoogleTable>` components at your disposal as well as the highly versatile `connectToSpreadsheet` helper. 
+
+##Initialization
+Initialise the data like so:
+You may use either `apiKey` or `clientId`. Please note that for data to be accessible using just the API Key the spreadsheet sharing must be set so that anyone with the link can view.
+```jsx harmony
+import ReactGoogleSheetConnector from "react-google-sheet-connector"
+
+<ReactGoogleSheetConnector clientid={YOUR_CLIENT_ID}
+    apiKey={YOUR_API_KEY}
+    spreadsheetId={YOUR_SPREADSHEET_ID}
+    spinner={ <div className="loading-spinner"/> } >
+    <div>
+    	This content will be rendered once the data has been fetched from the spreadsheet.
+    </div>
+</ReactGoogleSheetConnector>
+```
+
+###connectToSpreadsheet
+The following example shows how to connect a component to the Spreadsheet. The component should be passed as an argument into the `connectToSpreadsheet` function included in this module.
+```jsx harmony
+import { connectToSpreadsheet } from "react-google-sheet-connector"
+
+const MyComponent = (props) => {
+    return (
+        <div>
+            {
+                props.getSheet("Sheet Name")
+                    .map((row, i) =>
+                        JSON.stringify(row)
+                    )
+            }        
+        </div>
+    )
+}
+
+export default connectToSpreadsheet(MyComponent) 
+```
+This exposes the `getSheet` method to the component's props. `getSheet` takes one argument which is the name of the sheet and return an instance of `SheetData`.
+
+##SheetData
+This is the object that is used to operate on and render data. `SheetData` contains methods to help you process and render the data. These methods are as follows:
+
+* ####filter
+This method takes a single object as an argument. The keys should be the camel-case names of the column title and the values should be the respective values of those columns. For example, to get the rows where the "Item Category" column is equal to "Sports & Leisure":
+
+`.filter({itemCategory: "Sports & Leisure"})`
+
+This method returns the `SheetsData` instance for chaining.
+
+* ####group
+This method accepts one argument which is the title of the column you wish to group the data by. When grouped the data is split into an array of objects containing `name` and `values` properties. The `name` is the value for that particular group and the `values` the corresponding rows. E.g.
+
+`.group("Item Category")`
+
+This method returns the `SheetsData` instance for chaining.
+
+* ####sort
+This method accept one argument which is the title of the column you with to sort the data by. If your data has already been grouped then each group's `values` will be sorted by the specified column. E.g.
+
+`.sort("Item Category")`
+
+This method returns the `SheetData` instance for chaining.
+
+* ####reverse
+This method reverses the data and returns the `SheetData` instance.
+
+* ####map
+Use this as you would a regular array, once you have processed and are ready to render your data. Each row contains indexed values, as a regular array and also key-value pairs where the key is the column title converted to camelCase. 
+
+* ####getData
+This method returns an array of the original data rows from the sheet where each row contains indexed values and key-value pairs
+ 
+* ####getCurrentData
+This method returns the data after other methods e.g. `filter` and `group` have been applied.
+
+###Method Chaining
+Methods can be chained before finally rendering using `.map`. Here's an example:
+```jsx harmony
+props.getSheet("Sheet 1")
+    .filter({inStock: "y"})
+    .group("Product Type")
+    .sort("Product Name")
+    .reverse()
+    .map((group, i) => 
+        <div className="product-group" key={i}>
+            <h2>{group.name}</h2>
+            {
+                group.values.map((row, j) =>
+                    <div className="product-row" key={j}>
+                )
+            }
+        </div>    
+    )
+```
+
+##Components
+
+###GoogleSheet
+The `<GoogleSheet>` component injects data from the given sheet into the component specified as its `child` prop. You may also specify filter, group and sort props which work as described above.
+```jsx harmony
+import { GoogleSheet } from "react-google-sheet-connector"
+
+<GoogleSheet child={ChildComponent} sheetName="My Sheet" filter={{key: "Value"}} group="Column Title" sort="Column to Sort">
+```
+
+###GoogleRoute
+This component should be used as the `component` prop in a React Router `<Route>` component. By setting additional props on the component data will be passed from the specified sheet into the `child` component. In the example below data from the "Sheet 1" sheet will be passed into the props of `<ChildComponent>`. You may also use the filter, group and sort props as described above.
+```jsx harmony
+import { GoogleRoute } from "react-google-sheet-connector"
+
+<Route path="/mypath" component={GoogleRoute} child={ChildComponent} sheetName="Sheet 1" />
+```
+
+###GoogleTable
+This component renders a sheet from your Spreadsheet into an HTML table. As well as the props in the example below you may also assign `id` and `className` attributes to the component as well as `filter` as per the GoogleSheet example.
+```jsx harmony
+import { GoogleTable } from "react-google-sheet-connector"
+
+<GoogleTable sheetName="Sheet 1" />
+```
--- index.js
+++ index.js
@@ -1,103 +1,15 @@
 var React = require("react");
-var fetch = require("isomorphic-fetch");
-var camelCase = require("camelcase");
-var gapi = require("./gapi");
-
-var sheetsData = [];
+var createReactClass = require("create-react-class");
+var GoogleSheetConnector = require("google-sheet-connector");
+var googleSheet;
 
 /* Components */
 
-var GoogleSheetConnector = React.createClass ({
+var ReactGoogleSheetConnector = createReactClass({
     getInitialState: function() {
         return {
             isFetching: true,
-            sheetsData: [],
-            currSheet: 0
         };
-    },
-
-    loadSheetsData: function(data) {
-        this.setState({numSheets: data.sheets.length});
-        data.sheets.forEach(function(sheet) { return this.loadSheetViaKey(sheet.properties.title); }, this);
-    },
-
-    loadSheetViaAuth: function(sheet) {
-        gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: this.props.spreadsheetId,
-            range: sheet.properties.title
-        }).then(function(response) {
-            var values = JSON.parse(response.body).values;
-            this.loadSheet(sheet.properties.title, values);
-        }.bind(this));
-    },
-
-    loadSheetViaKey: function(sheetName) {
-        var url = [
-            "https://sheets.googleapis.com/v4/spreadsheets/",
-            this.props.spreadsheetId,
-            "/values/",
-            sheetName,
-            "?key=",
-            this.props.apiKey
-        ].join("");
-
-        fetch(url)
-            .then(function(response) { return response.json(); })
-            .then(function(json) {
-                var values = json.values;
-                this.loadSheet(sheetName, values);
-            }.bind(this));
-    },
-
-    loadSheet: function(sheetName, values) {
-        var headerRow = values[0];
-        var dataRows = values.slice(1);
-        var keys = headerRow.map(function(value) {
-            return camelCase(value);
-        }, this);
-
-        sheetsData = sheetsData.concat({
-            name: sheetName,
-            header: headerRow,
-            keys: keys,
-            data: this.loadRowsData(keys, dataRows)
-        });
-
-        this.setState(function(prevState) {
-            var currSheet = prevState.currSheet + 1;
-            return {
-                currSheet: currSheet,
-                isFetching: currSheet < prevState.numSheets
-            };
-        });
-    },
-
-    loadRowsData: function(keys, values) {
-        return values.map(function(row) {
-
-            keys.forEach(function(key, i) {
-                row[key] = row[i];
-            });
-
-            return row;
-        });
-    },
-
-    initClient: function() {
-        gapi.client.init({
-            discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-            clientId: this.props.clientId,
-            scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
-        }).then(function () {
-
-            gapi.client.sheets.spreadsheets.get({
-                spreadsheetId: this.props.spreadsheetId
-            }).then(function (response) {
-                var sheets = JSON.parse(response.body).sheets;
-                this.setState({numSheets: sheets.length});
-                sheets.forEach(this.loadSheetViaAuth);
-            }.bind(this));
-        }.bind(this));
     },
 
     shouldComponentUpdate: function(props, state) {
@@ -105,43 +17,34 @@ var GoogleSheetConnector = React.createClass ({
     },
 
     componentDidMount: function() {
-        if (this.props.clientId) {
-            return gapi.load("client:auth2", this.initClient);
-        } else if (this.props.apiKey) {
-            var url = [
-                "https://sheets.googleapis.com/v4/spreadsheets/",
-                this.props.spreadsheetId,
-                "?key=",
-                this.props.apiKey
-            ].join("");
-
-            fetch(url).then(function(response) { return response.json(); })
-                .then(function(json) { return this.loadSheetsData(json); }.bind(this));
-        }
+        googleSheet = new GoogleSheetConnector({
+            apiKey: this.props.apiKey,
+            clientId: this.props.clientId,
+            spreadsheetId: this.props.spreadsheetId
+        }, function() {
+            this.setState({isFetching: false});
+        }.bind(this));
     },
 
     render: function() {
-        var msg = this.state.isFetching ? "Loading data from Spreadsheet" : "Data from Spreadsheet loaded successfully";
-        console.info(msg);
         return this.state.isFetching ?
             this.props.spinner || React.createElement("div", null, "Loading") :
-            this.props.children;
+            this.props.children || null;
     }
 });
 
 var GoogleRoute = function(props) {
-    console.log(props);
     return React.createElement(GoogleSheet, props.route);
 };
 
 var GoogleSheet = function(props) {
-    var sheetData = new SheetData(props.sheetName);
+    var sheetData = googleSheet.getSheet(props.sheetName);
 
     if (props.filter) sheetData.filter(props.filter);
     if (props.group) sheetData.group(props.group);
     if (props.sort) sheetData.sort(props.sort);
 
-    var newProps = {data: sheetData.currentData};
+    var newProps = {data: sheetData.getCurrentData()};
     for (var i in props) {
         newProps[i] = props[i]
     }
@@ -149,21 +52,38 @@ var GoogleSheet = function(props) {
 };
 
 var GoogleTable = function(props) {
-    var sheetData = new SheetData(props.sheetName);
-    if (props.filter) sheetData.filter(props.filter);
+    var sheetData = googleSheet.getSheet(props.sheetName);
+    var rows = [];
 
-    return sheetData.toTable({
-        id: props.id,
-        className: props.className,
-        hasHeader: props.hasHeader
-    });
+    if (props.filter) {
+        sheetData.filter(props.filter);
+    }
+
+    if (props.hasHeader !== false) {
+        rows.push(renderTableRow(sheetData.header, "h", true));
+    }
+
+    rows = rows.concat(sheetData.getCurrentData().map(renderTableRow));
+
+    return React.createElement("table", {id: props.id, className: props.className},
+        React.createElement("tbody", null, rows)
+    );
 };
+
+function renderTableRow(row, key, isHeader) {
+    var tag = isHeader === true ? "th" : "td";
+
+    return React.createElement("tr", {key: "r" + key}, row.map(function(value, i) {
+            return React.createElement(tag, {key: "c" + i}, value);
+        })
+    );
+}
 
 var connectToSpreadsheet = function(component) {
     return function(props) {
         var newProps = {
             getSheet: function(sheetName) {
-                return new SheetData(sheetName);
+                return googleSheet.getSheet(sheetName);
             }
         };
 
@@ -175,115 +95,7 @@ var connectToSpreadsheet = function(component) {
     };
 };
 
-/* Helpers */
-
-function SheetData(sheetName) {
-    var sheet = sheetsData.find(function(sheet) { return sheet.name === sheetName; }) || {data: [], values: []};
-    this.data = sheet.data;
-    this.header = sheet.header;
-    this.keys = sheet.keys;
-    this.currentData = this.data.slice();
-}
-
-SheetData.prototype = {
-    map: function(callback) {
-        return this.currentData.map(callback);
-    },
-    filter: function(filterObj, strValue) {
-        this.currentData = this.data.filter(function(row) {
-            if (typeof filterObj === "object") {
-                for (var i in filterObj) {
-                    if (!row.hasOwnProperty(i) || row[i] !== filterObj[i]) {
-                        return false;
-                    }
-                }
-            } else {
-                const colIndex = this.header.indexOf(filterObj);
-                if (row[colIndex] !== strValue) return false;
-            }
-
-            return true;
-        }, this);
-
-        return this;
-    },
-    group: function(colName, sort) {
-        var groups = [];
-        var colIndex = this.header.indexOf(colName);
-
-        if (colIndex === -1) return this;
-
-        this.currentData.forEach(function(row) {
-            var groupName = row[colIndex];
-            var groupIndex = -1;
-
-            groups.forEach(function(group, i) {
-                if (group.name === groupName) groupIndex = i;
-            });
-
-            if (groupIndex > -1) {
-                groups[groupIndex].data.push(row);
-            } else {
-                groups.push({
-                    name: groupName,
-                    data: [row]
-                });
-            }
-        });
-
-        if (sort) sortArray(groups, "name");
-
-        this.currentData = groups;
-        this.dataIsGrouped = true;
-
-        return this;
-    },
-    sort: function(colName) {
-        if (this.dataIsGrouped) {
-            this.currentData.forEach(function(group) {
-                sortArray(group.data, camelCase(colName));
-            });
-        } else {
-            sortArray(this.currentData, camelCase(colName));
-        }
-        return this;
-    },
-    reverse: function() {
-        this.currentData.reverse();
-        return this;
-    },
-    toTable: function(options) {
-        function renderTableRow(row, key, isHeader) {
-            var tag = isHeader === true ? "th" : "td";
-
-            return React.createElement("tr", {key: "r" + key}, row.map(function(value, i) {
-                    return React.createElement(tag, {key: "c" + i}, value);
-                })
-            );
-        }
-
-        options = options || {};
-        var rows = [];
-        if (options.hasHeader !== false) rows.push(renderTableRow(this.header, "h", true));
-        rows = rows.concat(this.currentData.map(renderTableRow));
-
-        return React.createElement("table", {id: options.id, className: options.className},
-            React.createElement("tbody", null, rows)
-        );
-    }
-};
-
-function sortArray(array, orderBy) {
-
-    array.sort(function(a, b) {
-        var textA = a[orderBy] ? a[orderBy].toUpperCase() : "";
-        var textB = b[orderBy] ? b[orderBy].toUpperCase() : "";
-        if (textA < textB) return -1;
-        return textA > textB ? 1 : 0;
-    });
-}
-
-module.exports = GoogleSheetConnector;
+module.exports = ReactGoogleSheetConnector;
 module.exports.GoogleSheet = GoogleSheet;
 module.exports.GoogleRoute = GoogleRoute;
 module.exports.GoogleTable = GoogleTable;
